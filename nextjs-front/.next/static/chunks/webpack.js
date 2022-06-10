22150f489ad93eb377b3869d3cee2b61763befd2
juancarlossrp-ms <jjcarlos853@gmail.com>
2022-06-10 10:55:26 -0300

    'run'

--- nextjs-front/.next/static/chunks/webpack.js
+++ nextjs-front/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "52ec71e871b93fac"; }
+/******/ 		__webpack_require__.h = function() { return "e36627fa705e87fc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
